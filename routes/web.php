65414950126540d24f29e3e60842d404570dc42e
Rahul Baraiya <rahul.baraiya05@gmail.com>
2025-09-04 12:54:23 -0600

    created TicketController using resources

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\TicketController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
@@ -9,3 +10,7 @@ use Illuminate\Support\Facades\Route;
 Route::get('/{any}', function () {
     return file_get_contents(public_path('index.html'));
 })->where('any', '.*');
+
+Route::middleware(['web'])->group(function () {
+    Route::apiResource('tickets', TicketController::class);
+});
