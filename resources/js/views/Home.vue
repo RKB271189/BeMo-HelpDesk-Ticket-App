800ca581990ccf3de065b745b2dc041a3efedc85
Rahul Baraiya <rahul.baraiya05@gmail.com>
2025-09-04 11:00:02 -0600

    fresh setup

---
+++ resources/js/views/Home.vue
@@ -0,0 +1,9 @@
+<template>
+    <h1>Welcome To BeMo</h1>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style></style>
---
+++ vite.config.js
@@ -0,0 +1,19 @@
+import { defineConfig } from "vite";
+import vue from "@vitejs/plugin-vue";
+import path from "path";
+
+export default defineConfig({
+    root: "resources",
+    plugins: [vue()],
+    server: {
+        port: 5173,
+        strictPort: true,
+    },
+    build: {
+        outDir: path.resolve(__dirname, "public"),
+        emptyOutDir: true,
+        rollupOptions: {
+            input: "resources/js/main.js",
+        },
+    },
+});
