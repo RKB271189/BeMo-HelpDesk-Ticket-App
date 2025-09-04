fe2fefc51ccb12e763485454aa9380958503a8d8
Rahul Baraiya <rahul.baraiya05@gmail.com>
2025-09-04 12:22:13 -0600

    adding ticket note model and migration and aslo generating fake data for it

--- database/seeders/TicketSeeder.php
+++ database/seeders/TicketSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
 use App\Models\Ticket;
+use App\Models\TicketNote;
 
 class TicketSeeder extends Seeder
 {
@@ -13,6 +14,14 @@ class TicketSeeder extends Seeder
      */
     public function run(): void
     {
-        Ticket::factory()->count(30)->create();
+        Ticket::factory()->count(30)->create()->each(function ($ticket) {
+            $notesCount = rand(0, 3);
+
+            for ($i = 0; $i < $notesCount; $i++) {
+                TicketNote::factory()->create([
+                    'ticket_id' => $ticket->id
+                ]);
+            }
+        });
     }
 }
