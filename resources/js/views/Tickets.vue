ef04c7488c4595b216f0745adc67900b00b36e78
Rahul Baraiya <rahul.baraiya05@gmail.com>
2025-09-05 10:01:30 -0600

    added ticket card design

---
+++ resources/js/views/Tickets.vue
@@ -0,0 +1,18 @@
+<template>
+    <PageLayout>
+        <template #page_content>
+            <TicketCard />
+        </template>
+    </PageLayout>
+</template>
+
+<script>
+import PageLayout from './components/PageLayout.vue';
+import TicketCard from './components/TicketCard.vue';
+export default {
+    components: {
+        PageLayout,
+        TicketCard
+    }
+}
+</script>
