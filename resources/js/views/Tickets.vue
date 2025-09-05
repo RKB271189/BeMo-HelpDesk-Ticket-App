75d0dcaa97265dadccb4fff8c7cc711f048ac5b5
Rahul Baraiya <rahul.baraiya05@gmail.com>
2025-09-05 10:07:20 -0600

    added filter and pagination

--- resources/js/views/Tickets.vue
+++ resources/js/views/Tickets.vue
@@ -1,7 +1,38 @@
 <template>
     <PageLayout>
         <template #page_content>
+            <div class="ticket-filter">
+                <input type="text" class="ticket-filter__search" placeholder="Search tickets..." />
+                <select class="ticket-filter__category">
+                    <option value="">All Categories</option>
+                    <option value="Billing">Billing</option>
+                    <option value="Technical">Technical</option>
+                    <option value="General">General</option>
+                </select>
+                <button class="button button--danger">
+                    Clear
+                </button>
+            </div>
             <TicketCard />
+            <nav class="pagination">
+                <ul class="pagination__list">
+                    <li class="pagination__item pagination__item--disabled">
+                        <a href="#" class="pagination__link">« Prev</a>
+                    </li>
+                    <li class="pagination__item pagination__item--active">
+                        <a href="#" class="pagination__link">1</a>
+                    </li>
+                    <li class="pagination__item">
+                        <a href="#" class="pagination__link">2</a>
+                    </li>
+                    <li class="pagination__item">
+                        <a href="#" class="pagination__link">3</a>
+                    </li>
+                    <li class="pagination__item">
+                        <a href="#" class="pagination__link">Next »</a>
+                    </li>
+                </ul>
+            </nav>
         </template>
     </PageLayout>
 </template>
